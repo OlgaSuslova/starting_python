ef7c332bada2f2a42702322969eb7922b8d6959c
OlgaSuslova <alekssportmen@gmail.com>
2022-12-14 17:18:00 +0300

    added new files

---
+++ lec1.py
@@ -0,0 +1,107 @@
+# value = None
+# print (type(value))
+# a = 123
+# b = 1.23
+# print (type(a))
+# print (type(b))
+# value = 12334
+# print (type(value))
+# s = 'hello world'
+
+# print(s) # вывод строки
+# print (a,'-', b, '-', s)
+# print ('{1} - {2} - {0}'.format(a, b, s))
+# print (f'{a} - {b} - {s}')
+
+# f = False
+# print (f)
+
+
+
+# list = ['1', '2', '3']
+# col = ['hello', 1, 2, 4.5, True]
+# print (list)
+# print (col)
+
+
+
+# print input
+# print ('Введите a')
+# a = float(input())
+# print('Введите b')
+# b = float(input())
+# print(a, ' + ' , b, ' = ',  a+b)
+# print('{} {}'.format(a, b))
+# print(f'{a}, {b}')
+
+
+# a = 1.31232
+# b = 3
+# c = round(a * b, 5)
+# print(c)
+
+
+# a = 3
+# a *= 5
+# print(a)
+
+
+# a = 1 < 4 and 5 > 2 
+# print(a)
+
+# a = 1 != 2
+# print(a)
+
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же Маша!')
+# elif username == 'Марина':
+#     print('Ура, это же Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ')
+# else:
+#     print('Привет,', username)
+
+
+
+
+# original = 2345
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# print(inverted)
+
+
+
+
+# original = 2345
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+#     print(original)
+# else:
+#     print('пожалуй,')
+#     print('хватит) ')
+
+# print(inverted)
+
+
+# list = [1, 2, 3, 4, 5]
+# for i in list:
+#     print(i**2)
+
+
+r = range(10)
+for i in r:
+    print(i)
