489377e34334df806609903c3332f47123096a4e
OlgaSuslova <alekssportmen@gmail.com>
2022-12-21 16:56:18 +0300

    changed 1

--- 19.12/1.py
+++ 19.12/1.py
@@ -8,9 +8,12 @@
 
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(list)
+
 sum = 0
+
 for i in range(len(list)):
-    if i%2 !=0:
+    if i%2 != 0:
         sum += list[i]
+
 print(f'Сумма элементов, стоящих на нечетной позиции: -> {sum}')
 
