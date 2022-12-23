0f9bc2876a894d965e965a58b1c3b78e382ae120
OlgaSuslova <alekssportmen@gmail.com>
2022-12-23 15:13:35 +0300

    add

--- 19.12/4.py
+++ 19.12/4.py
@@ -17,3 +17,31 @@ while n != 0:
     n //= 2
 
 print(f'{number} -> {result[::-1]}')
+
+
+
+
+#решение учителя
+
+#4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+#
+# *Пример:*
+#
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
+
+# def dec_to_bin(num, result = ""):
+#   if num == 0:
+#       return result[::-1]
+#   result += str(num%2)
+#   return dec_to_bin(num//2,result)
+#
+#
+# n = int(input())
+#
+
+# if n!=0:
+# print(dec_to_bin(n))
+# else:
+# print(0)
--- 19.12/5.py
+++ 19.12/5.py
@@ -21,3 +21,21 @@ for i in range(k-1):
 
 
 print(f'k = {k} => {negative_list[::-1] + positive_list[1::]}')
+
+
+
+# решение учителя
+# def postive_fib(n):
+#   postive_list = [0,1]
+#   for i in range(n-1):
+#       postive_list.append(postive_list[-2]+postive_list[-1])
+#   return postive_list
+#
+# def negative_fiv(n):
+#   negative_list = [0,1]
+#   for i in range(n-1):
+#       negative_list.append(negative_list[-2]-negative_list[-1])
+#   return negative_list
+#
+#
+# print(negative_fiv(8)[::-1] + postive_fib(8)[1:])
