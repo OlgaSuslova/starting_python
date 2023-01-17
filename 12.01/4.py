19f9170b1214a228fa06e0bbe4940d0c2e25d787
OlgaSuslova <alekssportmen@gmail.com>
2023-01-17 14:48:47 +0300

    update

--- 12.01/4.py
+++ 12.01/4.py
@@ -14,5 +14,20 @@
     # Модуль восстановления работет в обратную сторону - из строки выходных данных, получить строку входных данных.
 
 
+text = 'AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE'
+text2 = ''
+text3 = ''
+count = 1
 
+for i in range(0, len(text)-1):
+    if text[i] == text[i+1]:
+        count +=1
+        text2 = str(count) + text[i] 
+    else: 
+        text2 = str(count) + text[i] 
+        count = 1
+        text3 += text2
+text3 += text2
+
+print(text3)
 
